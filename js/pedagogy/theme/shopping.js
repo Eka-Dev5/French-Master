32eb0042bdf92525f856b78b818651983a2cf8bc
Eka-Dev5 <blempro13@gmail.com>
2026-05-19 23:03:15 +0200

    Update shopping.js

--- js/pedagogy/theme/shopping.js
+++ js/pedagogy/theme/shopping.js
@@ -1,4 +1,24 @@
 // shopping.js
 // French Master
 
-export default {};
+"use strict";
+
+const SHOPPING_THEME = {
+
+    id: 6,
+
+    key: "shopping",
+
+    name: "Shopping",
+
+    emoji: "🏬",
+
+    color: "#dd6b20",
+
+    lessons: [],
+
+    vocabulary: [],
+
+    questions: []
+
+};
