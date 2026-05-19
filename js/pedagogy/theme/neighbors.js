0192e67aaf84f234fc88aa2b15021938e3390939
Eka-Dev5 <blempro13@gmail.com>
2026-05-19 23:02:05 +0200

    Update neighbors.js

--- js/pedagogy/theme/neighbors.js
+++ js/pedagogy/theme/neighbors.js
@@ -1,4 +1,24 @@
 // neighbors.js
 // French Master
 
-export default {};
+"use strict";
+
+const NEIGHBORS_THEME = {
+
+    id: 4,
+
+    key: "neighbors",
+
+    name: "Neighbors",
+
+    emoji: "🏘️",
+
+    color: "#805ad5",
+
+    lessons: [],
+
+    vocabulary: [],
+
+    questions: []
+
+};
