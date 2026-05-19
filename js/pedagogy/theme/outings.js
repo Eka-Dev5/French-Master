8e399338f9db2058066c82441816be8dce0978a8
Eka-Dev5 <blempro13@gmail.com>
2026-05-19 23:03:34 +0200

    Update outings.js

--- js/pedagogy/theme/outings.js
+++ js/pedagogy/theme/outings.js
@@ -1,4 +1,24 @@
 // outings.js
 // French Master
 
-export default {};
+"use strict";
+
+const OUTINGS_THEME = {
+
+    id: 7,
+
+    key: "outings",
+
+    name: "Outings",
+
+    emoji: "👫",
+
+    color: "#319795",
+
+    lessons: [],
+
+    vocabulary: [],
+
+    questions: []
+
+};
